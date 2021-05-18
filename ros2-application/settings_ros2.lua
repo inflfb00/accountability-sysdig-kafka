29f1f5fa1404af85540a7d5b68e57203965eaabe
inflfb00 <83959388+inflfb00@users.noreply.github.com>
2021-05-18 18:45:21 +0200

    Create settings_ros2.lua

---
+++ ros2-application/settings_ros2.lua
@@ -0,0 +1,20 @@
+-- Process, syscalls and data filters for ROS2 framework
+config = {
+   filter="(" ..
+	" proc.name=ros2" ..
+	" or proc.name contains gzclient" ..
+	" or proc.name contains gzserver" ..
+	" or proc.name contains rviz2" ..
+	" or proc.name contains talker" ..
+	" or proc.name=listener)" ..
+	" and (evt.type=write" ..
+	")" ..
+	" and (" ..
+	" evt.arg.data contains INFO" ..
+	" or evt.arg.data contains ERROR" ..
+	" or evt.arg.data contains WARN" ..
+	" or evt.arg.data contains DEBUG" ..
+    " or evt.arg.data contains File" ..
+	" or evt.arg.fd contains :" ..
+	")"
+}return config
